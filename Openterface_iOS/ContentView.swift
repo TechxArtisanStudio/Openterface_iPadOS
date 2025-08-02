53f04c307e5274559b1362e0cd89d708674f400e
Kevin Peng <kevin@beets3d.cn>
2025-08-02 22:33:07 +0800

    Initial Commit

---
+++ Openterface_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Openterface_iOS
+//
+//  Created by 彭志坚 on 8/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Openterface_iOS/Openterface_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Openterface_iOSApp.swift
+//  Openterface_iOS
+//
+//  Created by 彭志坚 on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Openterface_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
